a07d700175ed37d50e7c2e9e69b336a3c87ab595
stefanos1316 <stefanos1316@gmail.com>
2020-08-06 00:14:07 +0200

    More tests with batch files

--- batchExecution.sh
+++ batchExecution.sh
@@ -3,7 +3,7 @@
 [[ -d tmp ]] || mkdir -p tmp
 
 while read fileLine; do
-	node index -i $readLine -o issue_${readLine}.csv
+	node index -i $fileLine -o tmp/issue_${fileLine}.csv
 done <issues
 
 echo Done with all issues, please check files under the temp directory!
--- index.js
+++ index.js
@@ -41,12 +41,13 @@ program
             .end(function(err, res){
                 var element = res.body.rss.channel[0].item[0];
 
-                firstLine = 'Title, Type, Priority, Affected Version/s, Component/s, Labels,' 
-                    + 'Status, Resolution, Assignee, Reporter, Created, Updated, Resolved,'
-                    + 'Summary, Link, Votes, Description, Patch Info, Estimated Complexity';
+                firstLine = 'Title, Type, Priority, Component/s, Labels,' 
+                    + 'Status, Resolution, Assignee, Reporter, Created, Updated,'
+                    + 'Summary, Link, Votes, Description, Estimated Complexity';
                 
                 // Check if the any of the following elements are missing
-                'version' in element ? firstLine += ', Affected Version/s': logger.info('[CAMEL-' + program.issue + "] affected version/s missing.");
+                'version' in element ? firstLine += ', Affected Version/s' : logger.info('[CAMEL-' + program.issue + "] affected version/s missing.");
+                'resolved' in element ? firstLine += ', Resolved' : logger.info('[CAMEL-' + program.issue + "] resolved missing.");
                 if ('comments' in element) {
                     for (var i = 0 ; i < element.comments[0].comment.length; ++i)
                         firstLine += ', Comment_' + (i + 1);
@@ -54,30 +55,36 @@ program
                     logger.info('[CAMEL-' + program.issue + "] comments are missing.");
                 }
 
-                secondLine = element.title[0] + ', ' + element.type[0]['']  + ', ' +  element.priority[0]['_']
-                    + ', ' + element.version[0] + ', ' + element.component[0] + ', ' + element.labels[0].replace(/\r?\n|\r/g, " ")
+                secondLine = element.title[0] + ', ' + element.type[0]['_']  + ', ' +  element.priority[0]['_']
+                    + ', ' + element.component[0] + ', ' + element.labels[0].replace(/\r?\n|\r/g, " ")
                     + ', ' + element.status[0]['_'] + ', ' + element.resolution[0]['_'] + ', ' + element.assignee[0]['_']
                     + ', ' + element.reporter[0]['_'] + ', ' + element.created[0] + ', ' + element.updated[0]
-                    + ', ' + element.resolved[0] + ', ' + element.summary[0] + ', ' + element.link[0] + ', ' + element.votes[0]
-                    + ', ' + element.description[0].replace(/<[^>]*>?/gm, '').replace(/\r?\n|\r/g, " ").replace(/ +(?= )/g,'')
-                    + ', ' + element.customfields[0].customfield[7].customfieldvalues[0].customfieldvalue[0]['_']
-                    + ', ' + element.customfields[0].customfield[4].customfieldvalues[0].customfieldvalue[0]['_'];
+                    + ', ' + element.summary[0] + ', ' + element.link[0] + ', ' + element.votes[0]
+                    + ', ' + element.description[0].replace(/<[^>]*>?/gm, '').replace(/\r?\n|\r/g, " ").replace(/ +(?= )/g,'');
 
                 // Check if the any of the following elements are missing are log
+                if ('version' in element) {
+                    secondLine += ', ' + element.version[0];
+                }
+
+                if ('resolved' in element) {
+                    secondLine += ', ' + element.resolved[0];
+                }
+
                 if ('comments' in element) {
                     for (var i = 0 ; i < element.comments[0].comment.length; ++i)
                         secondLine += ', ' + element.comments[0].comment[i]['_'].replace(/<[^>]*>?/gm, '').replace(/\r?\n|\r/g, " ").replace(/ +(?= )/g,'');
                 }
 
                 // Write to defined file if was option given
-                if (program.ouput) {
+                if (!program.ouput) {
                     try {
-                        if (fs.existsSync(path)) {
+                        if (fs.existsSync(program.output)) {
                           //if file exists delete
-                          fs.unlinkSync(path) 
+                          fs.unlinkSync(program.output);
                         }
                       } catch(err) {
-                        console.error(err)
+                        console.error(err);
                       }
                     fs.appendFileSync(program.output, firstLine);
                     fs.appendFileSync(program.output, secondLine);
@@ -85,10 +92,10 @@ program
                     try {
                         if (fs.existsSync('output.csv')) {
                           // If file exists delete
-                          fs.unlinkSync('output.csv') 
+                          fs.unlinkSync('output.csv');
                         }
                       } catch(err) {
-                        console.error(err)
+                        console.error(err);
                       }
                     fs.appendFileSync('output.csv', firstLine);
                     fs.appendFileSync('output.csv', secondLine);
