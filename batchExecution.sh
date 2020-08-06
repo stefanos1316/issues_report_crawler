1bf7bd5eb4109bf2863a48e9e3ec74cc7e49dbd4
stefanos1316 <stefanos1316@gmail.com>
2020-08-06 21:42:53 +0200

    Automate issues collection process

--- batchExecution.sh
+++ batchExecution.sh
@@ -6,7 +6,9 @@ echo Create log files.
 
 while read fileLine; do
 	node index -i $fileLine -o tmp/issue_${fileLine}.csv
-done <issues
+done <<< $(curl -s https://issues.apache.org/jira/projects/CAMEL/issues/CAMEL-13590?filter=allopenissues \
+	| grep -o -P '(?<=issueKeys).*(?=jiraHasIssues)' | sed 's/,/\n/g' | sed 's/\\"//g' | sed 's/CAMEL\-//g' \
+	| sed 's/]//g' | sed 's/:\[//g' | sed '$d')
 
 echo Done with all issues, please check files under the temp directory.
 
